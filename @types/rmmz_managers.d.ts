043122f813cab2abfb935a324519beb2806cd6a4
plasma <6374339+elleonard@users.noreply.github.com>
2023-08-06 21:32:47 +0900

    DataManager.isXXXXの引数の型

--- @types/rmmz_managers.d.ts
+++ @types/rmmz_managers.d.ts
@@ -79,10 +79,10 @@ declare class DataManager {
 
   public static isBattleTest(): boolean;
   public static isEventTest(): boolean;
-  public static isSkill(item: DataManager.NoteHolder|null): item is MZ.Skill;
-  public static isItem(item: DataManager.NoteHolder|null): item is MZ.Item;
-  public static isWeapon(item: DataManager.NoteHolder|null): item is MZ.Weapon;
-  public static isArmor(item: DataManager.NoteHolder|null): item is MZ.Armor;
+  public static isSkill(item: DataManager.DrawableItem|DataManager.NoteHolder|null): item is MZ.Skill;
+  public static isItem(item: DataManager.DrawableItem|DataManager.NoteHolder|null): item is MZ.Item;
+  public static isWeapon(item: DataManager.DrawableItem|DataManager.NoteHolder|null): item is MZ.Weapon;
+  public static isArmor(item: DataManager.DrawableItem|DataManager.NoteHolder|null): item is MZ.Armor;
 
   public static createGameObjects(): void;
   public static setupNewGame(): void;
